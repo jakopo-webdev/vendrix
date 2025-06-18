f7376d36885beacacc082c8d1efd1ddd3293513a
jakopo-webdev <jakopozhegrova.business@gmail.com>
2025-06-18 13:24:09 +0200

    loading time decreased

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 export default async function Home() {
-  await delay(5000);
+  await delay(1000);
   return (
     <div>
       <Button>Do not click me</Button>
