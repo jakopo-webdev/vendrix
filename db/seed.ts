f20b068d9dc68671be47108e7dc4a3f19de62475
jakopo-webdev <jakopozhegrova.business@gmail.com>
2025-06-19 12:13:00 +0200

    sample data seeded into the db

---
+++ db/seed.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "../lib/generated/prisma";
+import sampleData from "@/db/sample-data";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Delete all products
+  await prisma.product.deleteMany({});
+
+  // Create products from sampleData
+  for (const product of sampleData.products) {
+    await prisma.product.create({
+      data: {
+        name: product.name,
+        slug: product.slug,
+        category: product.category,
+        description: product.description,
+        images: product.images,
+        price: product.price,
+        brand: product.brand,
+        rating: product.rating,
+        numReviews: product.numReviews,
+        stock: product.stock,
+        isFeatured: product.isFeatured,
+        banner: product.banner,
+      },
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
