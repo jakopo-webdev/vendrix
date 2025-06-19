2d89a7b3b3783f60df0027c30fa7406951f68961
jakopo-webdev <jakopozhegrova.business@gmail.com>
2025-06-19 09:27:43 +0200

    testing loading delay commented out

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import { ProductList } from "@/components/shared/product/product-list"
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+// const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 export default async function Home() {
-  await delay(1000);
+  // await delay(1000);
   return (
     <div className="space-y-8 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
       <ProductList />
