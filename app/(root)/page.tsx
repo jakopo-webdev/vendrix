d5e00242ce00bad5153d998bde27f6e0b0a6ad82
jakopo-webdev <jakopozhegrova.business@gmail.com>
2025-06-19 15:59:59 +0200

    logging of products removed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import { getProducts } from "@/lib/actions/product.actions";
 import { ProductList } from "@/components/shared/product/product-list";
-import { log } from "console";
 
 export default async function Home() {
   const products = await getProducts();
@@ -10,7 +9,6 @@ export default async function Home() {
     rating: product.rating.toString(),
     createdAt: String(product.createdAt),
   }));
-  log("Formatted Products:", formattedProducts);
 
   return (
     <div className="space-y-8 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
