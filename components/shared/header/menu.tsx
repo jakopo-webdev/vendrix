c187e6b053074012e5d256388bf2ea41636295e6
jakopo-webdev <jakopozhegrova.business@gmail.com>
2025-06-19 09:26:24 +0200

    typo corrected

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -40,7 +40,7 @@ export function Menu() {
               </Link>
               <Link href="/sign-in">
                 <Button className="w-full justify-start mb-2">
-                  <LogIn /> Sign Inn
+                  <LogIn /> Sign In
                 </Button>
               </Link>
               <ModeToggle />
